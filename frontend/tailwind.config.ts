1750555dbe83f31eaedb9645ad4ff1a4712ad521
LucidNg <94208867+LucidNg@users.noreply.github.com>
2024-06-29 23:29:54 +0700

    add assets and metadata adjustment
    Daisy UI themes, website's logo

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,6 +15,36 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ebebd3",
+          "primary-content": "#1e1e1e",
+          "secondary": "#f4d35e",
+          "secondary-content": "#083d77",
+          "accent": "#d72638",
+          "accent-content": "#fffbfa",
+          "neutral": "#fffbfa",
+          "neutral-content": "#ebebd3",
+          "base-100": "#fffbfa",
+          "base-200": "#ebebd3",
+          "base-300": "#bebebe",
+          "base-content": "#1e1e1e",
+          "info": "#10AD53",
+          "info-content": "#fffbfa",
+          "success": "#00ff00",
+          "success-content": "#001600",
+          "warning": "#00ff00",
+          "warning-content": "#001600",
+          "error": "#ff0000",
+          "error-content": "#160000",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 };
 export default config;
