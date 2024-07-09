1eb82aced4c185c670114ac83e2ef2cd3e3595d2
LucidNg <94208867+LucidNg@users.noreply.github.com>
2024-07-09 16:41:35 +0700

    card adjust

--- frontend/src/app/(home)/homePageComponent/Card.tsx
+++ frontend/src/app/(home)/homePageComponent/Card.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 export default function Card () {
     return (
-        <div className="bg-primary flex flex-col h-[400px] w-72">
-            <Image src="" alt="Card" width={150} height={150} className="p-5 min-w-72" />
+        <div className="bg-primary flex flex-col h-[410px] w-72">
+            <Image src="" alt="Card" width={150} height={150} layout="responsive" className="p-5 min-w-72 object-cover" loading="lazy"/>
             <span className="text-base-content font-semibold w-72 px-5 text-lg">Air Jordan 4 Retro {"Military Blue"}</span>
 
             <div className="flex flex-row items-center justify-between w-72 px-5 mb-5">
                 <span className="text-base-content text-lg">đ 4.956.000</span>
-                <button className="btn bg-secondary rounded-none text-neutral font-semibold text-xl min-w-32">Xem</button>
+                <button className="btn bg-secondary rounded-none text-neutral font-semibold text-xl min-w-32 hover:bg-accent hover:text-primary">Xem</button>
             </div>
         </div>
     )
--- frontend/src/app/(home)/page-client.tsx
+++ frontend/src/app/(home)/page-client.tsx
@@ -6,13 +6,13 @@ export default function Home({
     children1: React.ReactNode,
   }) {
     return (
-      <div className="px-10 py-5 bg-base-100">
+      <div className="px-16 py-10 bg-base-100">
         <div>
           <div className="pb-4">
-            <span className="text-primary font-bold text-2xl">Most liked</span>
+            <span className="text-accent font-bold text-4xl">Sản phẩm bán chạy</span>
           </div>
           <div className="flex flex-row w-full align-middle justify-between items-center">
-            <div id="most-liked" className="w-[90%] flex flex-row gap-10 overflow-x-scroll scroll-smooth p-2">
+            <div id="most-liked" className="w-[90%] flex flex-row gap-10 p-2">
               {children1}
             </div>
           </div>
