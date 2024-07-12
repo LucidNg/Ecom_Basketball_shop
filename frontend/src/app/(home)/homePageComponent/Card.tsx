839f1421ca0aaaacd4bc5cd71c46656994b9fc69
LucidNg <94208867+LucidNg@users.noreply.github.com>
2024-07-13 00:08:45 +0700

    update on frontend

--- frontend/src/app/(home)/homePageComponent/Card.tsx
+++ frontend/src/app/(home)/homePageComponent/Card.tsx
@@ -3,37 +3,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { FetchProduct, Product } from "../../../lib/product";
 
-<<<<<<< Updated upstream
-export default function Card () {
-    return (
-        <>
-        {products.map(product => (
-          <div key={product.productID} className="bg-primary flex flex-col h-[400px] w-72">
-            <Image 
-              src={product.imageURL} 
-              alt={product.productName} 
-              width={150} 
-              height={150} 
-              className="p-5 min-w-72 object-cover"
-              loading="lazy" 
-            />
-            <span className="text-base-content font-semibold w-72 px-5 text-lg">
-              {product.productName}
-            </span>
-            <div className="flex flex-row items-center justify-between w-72 px-5 mb-5">
-              <span className="text-base-content text-lg">
-                ${product.price}
-              </span>
-              <button className="btn bg-secondary rounded-none text-neutral font-semibold text-xl min-w-32 hover:bg-accent hover:text-primary">
-                Xem
-              </button>
-            </div>
-          </div>
-        ))}
-      </>
-    )
-}
-=======
 interface CardProps {
   limit?: number;
 }
@@ -83,4 +52,3 @@ export default async function Card({ limit }: CardProps) {
     </>
   );
 }
->>>>>>> Stashed changes
--- frontend/src/app/(home)/homePageComponent/TopBar.tsx
+++ frontend/src/app/(home)/homePageComponent/TopBar.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { useState, useEffect } from 'react';
 import Image from "next/image";
 import Link from "next/link";
 import {Search} from "@geist-ui/icons"
