c1042c5de93a2d9a6e81eac7909dc97883099561
pnguyenhere <phnguyen.pn001@gmail.com>
2024-07-12 23:23:38 +0700

    Reapply "update on server"
    This reverts commit 5c24761956de40feecf3ecbc665a53fa3247b793.

--- backend/database/query.go
+++ backend/database/query.go
@@ -1,124 +1,52 @@
 package database
 
 import (
-	"database/sql"
-	"encoding/json"
 	"net/http"
 
-	_ "github.com/mattn/go-sqlite3"
+	sqlitecloud "github.com/sqlitecloud/sqlitecloud-go"
 )
 
-type User struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-func QueryUsers(db *sql.DB, w http.ResponseWriter) error {
-	rows, err := db.Query("SELECT * FROM users")
+func QueryUsers(db *sqlitecloud.SQCloud, w http.ResponseWriter) error {
+	rows, err := db.Select("SELECT * FROM users")
 	if err != nil {
 		return err
 	}
-	defer rows.Close()
 
-	var users []User
-
-	for rows.Next() {
-		var user User
-		err = rows.Scan(&user.ID, &user.Email, &user.Password)
-		if err != nil {
-			return err
-		}
-		users = append(users, user)
-	}
-
-	if err := rows.Err(); err != nil {
+	defer rows.Dump()
+	_, err = w.Write([]byte(rows.ToJSON()))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return err
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(users); err != nil {
-		return err
-	}
-
 	return nil
 }
 
-type Category struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-}
-
-func QueryCategory(db *sql.DB, w http.ResponseWriter) error {
-	rows, err := db.Query("SELECT * FROM category")
+func QueryCategory(db *sqlitecloud.SQCloud, w http.ResponseWriter) error {
+	rows, err := db.Select("SELECT * FROM category")
 	if err != nil {
 		return err
 	}
-	defer rows.Close()
 
-	var categories []Category
-
-	for rows.Next() {
-		var category Category
-		err = rows.Scan(&category.ID, &category.Name, &category.Description)
-		if err != nil {
-			return err
-		}
-		categories = append(categories, category)
-	}
-
-	if err := rows.Err(); err != nil {
+	defer rows.Dump()
+	_, err = w.Write([]byte(rows.ToJSON()))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return err
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(categories); err != nil {
-		return err
-	}
-
 	return nil
 }
 
-type Product struct {
-	ProductID   string `json:"productID"`
-	CategoryID  string `json:"categoryID"`
-	ProductName string `json:"productName"`
-	Description string `json:"description"`
-	Brand       string `json:"brand"`
-	Price       int    `json:"price"`
-	Stock       int    `json:"stock"`
-	ImageURL    string `json:"imageURL"`
-	DateAdded   string `json:"dateAdded"`
-	Size        string `json:"size"`
-}
-
-func QueryProduct(db *sql.DB, w http.ResponseWriter) error {
-	rows, err := db.Query("SELECT * FROM product")
+func QueryProduct(db *sqlitecloud.SQCloud, w http.ResponseWriter) error {
+	rows, err := db.Select("SELECT * FROM product")
 	if err != nil {
 		return err
 	}
-	defer rows.Close()
 
-	var products []Product
-
-	for rows.Next() {
-		var product Product
-		err = rows.Scan(&product.ProductID, &product.CategoryID, &product.ProductName, &product.Description, &product.Brand, &product.Price, &product.Stock, &product.ImageURL, &product.DateAdded, &product.Size)
-		if err != nil {
-			return err
-		}
-		products = append(products, product)
-	}
-
-	if err := rows.Err(); err != nil {
+	defer rows.Dump()
+	_, err = w.Write([]byte(rows.ToJSON()))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return err
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(products); err != nil {
-		return err
-	}
-
 	return nil
 }
