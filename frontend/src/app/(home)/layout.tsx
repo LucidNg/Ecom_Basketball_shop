8bdc0217f9b8ed785168ef37392dfe39f4f05156
LucidNg <94208867+LucidNg@users.noreply.github.com>
2024-07-12 22:56:59 +0700

    Reapply "update on frontend"
    This reverts commit 5730c1586d04e6a73c795954a8a9e5ae39c32acb.

--- frontend/src/app/(home)/layout.tsx
+++ frontend/src/app/(home)/layout.tsx
@@ -1,9 +1,18 @@
+<<<<<<< Updated upstream
 import Banner from "./homePageComponent/Banner";
 import Navbar from "./homePageComponent/Navbar";
 import TopBar from "./homePageComponent/TopBar";
+=======
+import { ReactNode } from 'react';
+import Banner from './homePageComponent/Banner';
+import Navbar from './homePageComponent/Navbar';
+import TopBar from './homePageComponent/TopBar';
+import Footer from '../appComoponent/Footer';
+>>>>>>> Stashed changes
 
 
 
+<<<<<<< Updated upstream
 export default function RootLayout({
     children,
   }: Readonly<{
@@ -18,3 +27,17 @@ export default function RootLayout({
       </div>
     );
   }
+=======
+  return (
+    <div className="w-full bg-base-100">
+      <TopBar />
+      <Navbar />
+      <Banner />
+      {children}
+      <Footer/>
+    </div>
+  );
+};
+
+export default RootLayout;
+>>>>>>> Stashed changes
---
+++ frontend/src/app/product/page.tsx
@@ -0,0 +1,10 @@
+import Card from "../(home)/homePageComponent/Card";
+import Home from "./page-client";
+
+export default function Page() {
+  return (
+    <Home 
+    children1=<Card/>>
+    </Home>
+  );
+}
