e2061bd8298b41cc1f4ffe6f9f9b05baaad8f381
LucidNg <94208867+LucidNg@users.noreply.github.com>
2024-07-29 02:08:34 +0700

    bug fix

--- frontend/src/app/product/[productId]/page.tsx
+++ frontend/src/app/product/[productId]/page.tsx
@@ -1,13 +1,26 @@
-"use client"
-import Card from "@/app/[category]/productComponent/Card";
-import DetailedProductPageCli from "./page-client";
+'use client';
+import Card from '@/app/[category]/productComponent/Card';
+import { useState } from 'react';
+import DetailedProductPageCli from './page-client';
 
-export default function DetailedProductPage() {
+export default function ProductPage() {
+  const [hasMoreProducts, setHasMoreProducts] = useState(false);
 
+  const handleCheckHasMoreProducts = (hasMore: boolean) => {
+    setHasMoreProducts(hasMore);
+  };
 
-    return (
-      <DetailedProductPageCli 
-      children1={<Card limit={10} category={""}/>}>
-      </DetailedProductPageCli>
-    );
-  }
+  return (
+    <DetailedProductPageCli children1={
+      <Card
+        category=""
+        currentPage={1}
+        itemsPerPage={15}
+        checkHasMoreProducts={handleCheckHasMoreProducts}
+        sortBy=""
+        minPrice=""
+        maxPrice=""
+      />}>
+    </DetailedProductPageCli>
+  );
+}
