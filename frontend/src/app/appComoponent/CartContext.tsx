265120e2dbd6b773cd3d206bfd8b14b852b8f2ac
phungptn <ngothaiphung412@gmail.com>
2024-08-03 01:27:22 +0700

    Update CartContext.tsx
    update 2 functions to increase and decrease quantity in cart context

--- frontend/src/app/appComoponent/CartContext.tsx
+++ frontend/src/app/appComoponent/CartContext.tsx
@@ -2,13 +2,13 @@
 import React, { createContext, useState, useContext, ReactNode } from "react";
 import { IProduct } from "./ProductCard.type";
 import ProductCard from "./ProductCard";
-import { remove } from "lodash";
+import { remove, update } from "lodash";
 
 interface CartContextType {
   cart: IProduct[];
   addToCart: (product: IProduct) => void;
   removeFromCart: (productId: string | undefined) => void;
-  increseQuantity: (productId: string) => void;
+  increaseQuantity: (productId: string) => void;
   decreaseQuantity: (productId: string) => void;
 }
 
@@ -43,11 +43,52 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const removeFromCart = (productId: string | undefined) => {
     if (productId) {
       setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+      return cart;
+    } else throw new Error("Product id does not exist.");
+  };
+
+  const increaseQuantity = (productId: string) => {
+    if (productId) {
+      setCart((prevCart) => {
+        const existingProductIndex = prevCart.findIndex(
+          (item) => item.id === productId
+        );
+        if (existingProductIndex !== -1) {
+          const updatedCart = [...prevCart];
+          updatedCart[existingProductIndex].quantity += 1;
+          return updatedCart;
+        } else return prevCart;
+      });
+    } else throw new Error("Product id does not exist.");
+  };
+
+  const decreaseQuantity = (productId: string) => {
+    if (productId) {
+      setCart((prevCart) => {
+        const existingProductIndex = prevCart.findIndex(
+          (item) => item.id === productId
+        );
+        if (existingProductIndex !== -1) {
+          let updatedCart = [...prevCart];
+          if (updatedCart[existingProductIndex].quantity > 0) {
+            updatedCart[existingProductIndex].quantity -= 1;
+          } else updatedCart = removeFromCart(productId);
+          return updatedCart;
+        } else return prevCart;
+      });
     } else throw new Error("Product id does not exist.");
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+      }}
+    >
       {" "}
       {children}
     </CartContext.Provider>
