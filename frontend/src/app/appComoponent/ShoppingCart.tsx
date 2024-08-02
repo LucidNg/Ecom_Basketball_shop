6c7c6e8614d115627f094a8562fb9532b331b1a0
phungptn <ngothaiphung412@gmail.com>
2024-08-03 00:10:53 +0700

    Update ShoppingCart.tsx
    show up items that were added to cart

--- frontend/src/app/appComoponent/ShoppingCart.tsx
+++ frontend/src/app/appComoponent/ShoppingCart.tsx
@@ -1,46 +1,49 @@
 "use client";
 
 import React, { useState, useEffect, useCallback } from "react";
+import Link from "next/link";
 import ProductCard from "./ProductCard";
 import { IProduct } from "./ProductCard.type";
-import Link from "next/link";
+import { useCart } from "./CartContext";
 
-const cartItems: IProduct[] = [
-  {
-    id: "1",
-    name: "Jordan Air Globe T-Shirt Kids",
-    price: 587000,
-    quantity: 1,
-    image:
-      "https://i1.t4s.cz/products/95d121-001/jordan-air-globe-t-shirt-kids-749837-95d121-001.png",
-    size: "XS",
-  },
-  {
-    id: "2",
-    name: "adidas Basketball Select Tee White",
-    price: 789000,
-    quantity: 2,
-    image:
-      "https://www.cosmossport.gr/2869439-product_medium/adidas-basketball-select-tee.jpg",
-    size: "L",
-  },
-];
+// // mock data
+// const cartItems: IProduct[] = [
+//   {
+//     id: "1",
+//     name: "Jordan Air Globe T-Shirt Kids",
+//     price: 587000,
+//     quantity: 1,
+//     image:
+//       "https://i1.t4s.cz/products/95d121-001/jordan-air-globe-t-shirt-kids-749837-95d121-001.png",
+//     size: "XS",
+//   },
+//   {
+//     id: "2",
+//     name: "adidas Basketball Select Tee White",
+//     price: 789000,
+//     quantity: 2,
+//     image:
+//       "https://www.cosmossport.gr/2869439-product_medium/adidas-basketball-select-tee.jpg",
+//     size: "L",
+//   },
+// ];
 
 const ShoppingCart = () => {
+  const { cart, removeFromCart } = useCart();
   const [selectItems, setSelectItems] = useState<IProduct[]>([]);
   const [totalPrice, setTotalPrice] = useState<number>(0);
   const [isSelectAll, setIsSelectAll] = useState<boolean>(false);
   const [checkedItems, setCheckedItems] = useState<boolean[]>(
-    new Array(cartItems.length).fill(false)
+    new Array(cart.length).fill(false)
   );
 
   const handleSelectAll = () => {
-    const newCheckedItems = new Array(cartItems.length).fill(!isSelectAll);
+    const newCheckedItems = new Array(cart.length).fill(!isSelectAll);
     setCheckedItems(newCheckedItems);
     setIsSelectAll(!isSelectAll);
 
     if (!isSelectAll) {
-      setSelectItems(cartItems);
+      setSelectItems(cart);
     } else {
       setSelectItems([]);
     }
@@ -51,9 +54,9 @@ const ShoppingCart = () => {
     newCheckedItems[index] = !newCheckedItems[index];
     setCheckedItems(newCheckedItems);
 
-    const selected = cartItems.filter((_, idx) => newCheckedItems[idx]);
+    const selected = cart.filter((_, idx) => newCheckedItems[idx]);
     setSelectItems(selected);
-    setIsSelectAll(selected.length === cartItems.length);
+    setIsSelectAll(selected.length === cart.length);
   };
 
   const getTotalBill = useCallback((): number => {
@@ -71,7 +74,7 @@ const ShoppingCart = () => {
     <>
       <div className="flex flex-col gap-12">
         <div className="flex flex-col gap-6 h-max">
-          {cartItems.map((product, index) => (
+          {cart.map((product, index) => (
             <div
               key={product.id}
               className="flex flex-row items-center gap-20 px-16 py-6 bg-[#EBEBD5] h-full"
