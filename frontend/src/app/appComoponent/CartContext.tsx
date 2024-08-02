08bfb6bea4c083ef91d779cf8c6e5d346dc9f930
phungptn <ngothaiphung412@gmail.com>
2024-08-02 23:26:13 +0700

    Create CartContext.tsx

---
+++ frontend/src/app/appComoponent/CartContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+import { IProduct } from "./ProductCard.type";
+import ProductCard from "./ProductCard";
+
+interface CartContextType {
+  cart: IProduct[];
+  addToCart: (product: IProduct) => void;
+  removeFromCart: (productId: string) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart myust be used within a CartProvider");
+  }
+  return context;
+};
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<IProduct[] | []>([]);
+
+  const addToCart = (product: IProduct) => {
+    setCart((prevCart) => {
+      const existingProductIndex = prevCart.findIndex(
+        (item) => item.id === product.id
+      );
+      if (existingProductIndex !== -1) {
+        const updatedCart = [...prevCart];
+        updatedCart[existingProductIndex].quantity += product.quantity;
+        return updatedCart;
+      } else {
+        return [...prevCart, product];
+      }
+    });
+  };
+
+  const removeFromCart = (productId: string) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {" "}
+      {children}
+    </CartContext.Provider>
+  );
+};
